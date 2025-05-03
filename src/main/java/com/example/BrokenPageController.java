ccf8395d979b7d80bbc1cdea9128a99db4ea92ea
CloudFolksPublic <info@cloudfolkshub.com>
2025-05-03 12:43:31 +0530

    Update BrokenPageController.java

--- src/main/java/com/example/BrokenPageController.java
+++ src/main/java/com/example/BrokenPageController.java
@@ -9,13 +9,21 @@ public class BrokenPageController {
     @GetMapping("/broken")
     public String brokenPage() {
         return "<html>" +
-                "<head><title>Broken Page</title></head>" +
-                "<body style='text-align:center; background-color:#ffcccc;'>" +
-                "    <h1 style='color: red;'>This page is under construction</h1>" +
-                "    <p>Error: Button below does nothing. Form is missing.</p>" +
-                "    <button id='brokenButton'>Click Me</button>" +
+                "<head>" +
+                "    <title>CloudFolks HUB - Broken Page</title>" +
+                "</head>" +
+                "<body style='text-align:center; background-color:#fff8f0;'>" +
+                "    <h1 style='color: #FF5733;'>Welcome to <span style='color: #900C3F;'>Broken Page</span>!</h1>" +
+                "    <p style='font-size:20px; color: #555;'>This looks like a working form, but the button is broken.</p>" +
+                "    <form id='form' method='post' action='/submit' style='margin-top:20px;'>" +
+                "        <label for='name'>Name:</label><br>" +
+                "        <input type='text' id='name' name='name' required><br><br>" +
+                "        <label for='email'>Email:</label><br>" +
+                "        <input type='email' id='email' name='email' required><br><br>" +
+                "        <!-- This button is disabled or broken -->" +
+                "        <button type='submit' onclick='return false;'>Submit</button>" +
+                "    </form>" +
                 "</body>" +
                 "</html>";
     }
 }
-
