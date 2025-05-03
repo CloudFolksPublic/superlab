aff0585138b602c981943b5b90355c9e32964649
CloudFolksPublic <info@cloudfolkshub.com>
2025-05-03 12:07:56 +0530

    Create WebProjectApplication.java

---
+++ src/main/java/com/example/WebProjectApplication.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class WebProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebProjectApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<html>" +
+                "<head><title>CloudFolks HUB</title></head>" +
+                "<body style='text-align:center; background-color:#f0f8ff;'>" +
+                "<h1 style='color: #4CAF50;'>Welcome to <span style='color: #00008B;'>CloudFolks HUB</span>!</h1>" +
+                "<p style='font-size:20px; color: #555;'>Empowering Your DevOps Journey</p>" +
+                "<form method='post' action='/submit'>" +
+                "<label>Name:</label><input type='text' name='name' required><br><br>" +
+                "<label>Email:</label><input type='email' name='email' required><br><br>" +
+                "<button type='submit'>Submit</button>" +
+                "</form></body></html>";
+    }
+
+    @PostMapping("/submit")
+    public String submit(@RequestParam String name, @RequestParam String email) {
+        return "<html><head><title>Submitted</title></head>" +
+                "<body style='text-align:center; background-color:#f0f8ff;'>" +
+                "<h1>Thank You, " + name + "!</h1>" +
+                "<p>Your email (" + email + ") has been submitted successfully.</p></body></html>";
+    }
+}
